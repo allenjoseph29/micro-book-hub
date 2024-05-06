ab0c8873ccbdb6daad4c09948a27620dec28ef58
Allen Joseph <allen.joseph@universum-inkasso.de>
2024-05-06 20:17:06 +0200

    Added execute permissions for the mvnw script

--- catalog-service/src/main/java/com/allenlabs/microbookhub/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/allenlabs/microbookhub/catalog/CatalogServiceApplication.java
@@ -2,10 +2,10 @@ package com.allenlabs.microbookhub.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationProperties.class)
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
