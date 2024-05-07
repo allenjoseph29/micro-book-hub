3ee8a70d56203420c9aedf651fa5eae69f26592f
Allen Joseph <allen.joseph@universum-inkasso.de>
2024-05-07 20:58:48 +0200

    Added RabbitMQ configuration and setup

--- order-service/src/test/java/com/allenlabs/microbookhub/orders/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/allenlabs/microbookhub/orders/OrderServiceApplicationTests.java
@@ -5,7 +5,5 @@ import org.junit.jupiter.api.Test;
 class OrderServiceApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
--- order-service/src/test/java/com/allenlabs/microbookhub/orders/TestOrderServiceApplication.java
+++ order-service/src/test/java/com/allenlabs/microbookhub/orders/TestOrderServiceApplication.java
@@ -11,20 +11,21 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestOrderServiceApplication {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
 
-	@Bean
-	@ServiceConnection
-	RabbitMQContainer rabbitContainer() {
-		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    RabbitMQContainer rabbitContainer() {
+        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.from(OrderServiceApplication::main).with(TestOrderServiceApplication.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(OrderServiceApplication::main)
+                .with(TestOrderServiceApplication.class)
+                .run(args);
+    }
 }
